f6db1da113c23068fd1b25bff676b66609cb3684
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-05-02 01:11:41 +0300

    Remove var

--- src/main/java/sa/edu/kau/fcit/cpit252/lab4BuilderFactory/App.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/lab4BuilderFactory/App.java
@@ -3,13 +3,13 @@ package sa.edu.kau.fcit.cpit252.lab4BuilderFactory;
 public class App {
 
   public static void main(String[] args) {
-    var knight = new Knight("Aragorn");
+    Knight knight = new Knight("Aragorn");
     System.out.println(knight.toString());
 
-    var archer = new Archer("Legolas");
+    Archer archer = new Archer("Legolas");
     System.out.println(archer.toString());
     
-    var flagBearer = new FlagBearer("Frodo");
+    FlagBearer flagBearer = new FlagBearer("Frodo");
     System.out.println(flagBearer.toString());
   }
 }
